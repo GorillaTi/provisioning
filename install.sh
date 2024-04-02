75850439757df017321be63d0d370a695a3887fa
GorillaTi <ed.cespedesa@gmail.com>
2024-04-02 11:01:17 -0400

    upgrade configuration

--- install.sh
+++ install.sh
@@ -14,12 +14,27 @@
 # ------------------------------------------------------------------------------
 f_install() {
   local DIR_LOCAL
-  DIR_LOCAL="$PWD/provisionings"
-  if [ -n "$DIR_LOCAL" ]; then
-    echo "Directorio Provisioning no existe"
-  else
-    echo "Directorio Provisioning no existe"
-  fi
+  local SCRIPT_LOCAL
+  local STATUS_LOCAL
+  DIR_LOCAL="$PWD/provisioning"
+  SCRIPT_LOCAL="provisioning.sh"
+
+  while  [ "$STATUS_LOCAL" == 1 ]; do
+    if [ -n "$DIR_LOCAL" ]; then
+      echo "Directorio Provisioning no existe"
+      mkdir -p "$DIR_LOCAL"
+      STATUS_LOCAL=0
+    else
+      echo "Directorio Provisioning si existe"
+      if [ ! -f "$DIR_LOCAL/$SCRIPT_LOCAL" ]; then
+        curl -o "$DIR_LOCAL/$SCRIPT_LOCAL https://github.com/GorillaTi/Provisioning/raw/main/provisioning.sh"
+        STATUS_LOCAL=0
+      else
+        sh -c "($DIR_LOCAL/$SCRIPT_LOCAL)"
+        STATUS_LOCAL=1
+      fi
+    fi
+  done
 }
 # ------------------------------------------------------------------------------
 # SCRIPT
